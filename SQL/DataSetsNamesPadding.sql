fc073fc23ebc990ed044ce8ddb71c04acc65b0d6
Giacomo Caglioni <gcaglion@yahoo.com>
2018-12-11 20:20:21 +0100

    lowered MaxEpochs to 130 for largest trainsets

---
+++ SQL/DataSetsNamesPadding.sql
@@ -0,0 +1,2 @@
+update clientinfo set ActionXMLFile='C:\Users\gcaglion\dev\zzz/Config.NoInclude/DataSets/06000.xml' where actionxmlfile='C:\Users\gcaglion\dev\zzz/Config.NoInclude/DataSets/6000.xml';
+update clientinfo set ShapeXMLFile='C:\Users\gcaglion\dev\zzz/Config.NoInclude/DataShapes/050-1.xml' where ShapeXMLFile='C:\Users\gcaglion\dev\zzz/Config.NoInclude/DataShapes/50-1.xml';
