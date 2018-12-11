b517ed707df3f9ba1f2f2fb34a87db5710310884
Giacomo Caglioni <gcaglion@yahoo.com>
2018-12-11 19:56:04 +0100

    more tests

--- SQL/deletePid.sql
+++ SQL/deletePid.sql
@@ -2,7 +2,7 @@ set verify on
 undefine pid
 delete from CLIENTINFO where processid in(&&pid);
 delete from COREIMAGE_NN where processid in(&&pid);
-delete from CORELAYOUTS where processid in(&&pid);
+delete from CORELAYOUTS where enginepid in(&&pid);
 delete from ENGINECORES where enginepid in(&&pid);
 delete from ENGINES where processid in(&&pid);
 delete from RUNLOG where processid in(&&pid);
