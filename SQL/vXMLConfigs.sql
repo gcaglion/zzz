0585098b11748f7744d2241d3e626df0e24fca8e
Giacomo Caglioni <gcaglion@yahoo.com>
2018-12-25 23:43:14 +0100

    fixed bug in vXMLconfigs.sql

--- SQL/vXMLConfigs.sql
+++ SQL/vXMLConfigs.sql
@@ -6,7 +6,7 @@ select processid, NULL uHistoryLen, substr(parmval,1,256) uTopology, NULL MaxEpo
 union
 select processid, NULL uHistoryLen, NULL uTopology, substr(parmval,1,256) MaxEpochs, NULL uSampleLen, NULL uDate0  from xmlconfigs where upper(parmdesc) like '%MAXEPOCHS%'
 union
-select processid, NULL uHistoryLen, NULL uTopology, NULL MaxEpochs, substr(parmval,1,256) uSampleLen, NULL uDate0  from xmlconfigs where upper(parmdesc) like '%MAXEPOCHS%'
+select processid, NULL uHistoryLen, NULL uTopology, NULL MaxEpochs, substr(parmval,1,256) uSampleLen, NULL uDate0  from xmlconfigs where upper(parmdesc) like '%SAMPLELEN%'
 union
 select processid, NULL uHistoryLen, NULL uTopology, NULL MaxEpochs, NULL uSampleLen, substr(parmval,1,256) uDate0  from xmlconfigs where upper(parmdesc) like '%DATE0%'
 ) 
