3f7d876c52b052f0339638ffcef6d5cdeeecf04a
Giacomo Caglioni <gcaglion@yahoo.com>
2018-12-25 02:04:56 +0100

    Overview in Tester.xlsm now uses values from XMLconfigs

--- SQL/vXMLConfigs.sql
+++ SQL/vXMLConfigs.sql
@@ -1,11 +1,14 @@
 create or replace view vXMLconfig as
-select processid, parmval HistoryLen, NULL Topology, NULL MaxEpochs, NULL sampleLen, NULL Date0 from xmlconfigs where upper(parmdesc) like '%HISTORYLEN%'
+select processid, min(uHistoryLen) HistoryLen, min(uTopology) Topology, min(uSampleLen) SampleLen, min(uDate0) Date0 from (
+select processid, substr(parmval,1,256) uHistoryLen, NULL uTopology, NULL MaxEpochs, NULL uSampleLen, NULL uDate0 from xmlconfigs where upper(parmdesc) like '%HISTORYLEN%'
 union
-select processid, NULL HistoryLen, parmval Topology, NULL MaxEpochs, NULL sampleLen, NULL Date0  from xmlconfigs where upper(parmdesc) like '%LEVELRATIO%'
+select processid, NULL uHistoryLen, substr(parmval,1,256) uTopology, NULL MaxEpochs, NULL uSampleLen, NULL uDate0  from xmlconfigs where upper(parmdesc) like '%LEVELRATIO%'
 union
-select processid, NULL HistoryLen, NULL Topology, parmval MaxEpochs, NULL sampleLen, NULL Date0  from xmlconfigs where upper(parmdesc) like '%MAXEPOCHS%'
+select processid, NULL uHistoryLen, NULL uTopology, substr(parmval,1,256) MaxEpochs, NULL uSampleLen, NULL uDate0  from xmlconfigs where upper(parmdesc) like '%MAXEPOCHS%'
 union
-select processid, NULL HistoryLen, NULL Topology, NULL MaxEpochs, parmval SampleLen, NULL Date0  from xmlconfigs where upper(parmdesc) like '%MAXEPOCHS%'
+select processid, NULL uHistoryLen, NULL uTopology, NULL MaxEpochs, substr(parmval,1,256) uSampleLen, NULL uDate0  from xmlconfigs where upper(parmdesc) like '%MAXEPOCHS%'
 union
-select processid, NULL HistoryLen, NULL Topology, NULL MaxEpochs, NULL SampleLen, parmval Date0  from xmlconfigs where upper(parmdesc) like '%DATE0%'
+select processid, NULL uHistoryLen, NULL uTopology, NULL MaxEpochs, NULL uSampleLen, substr(parmval,1,256) uDate0  from xmlconfigs where upper(parmdesc) like '%DATE0%'
+) 
+group by processid
 ;
