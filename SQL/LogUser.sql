7d844ad21c9fbb2ef44f0c027439db1f70f798ec
Giacomo Caglioni <gcaglion@yahoo.com>
2019-01-15 14:08:23 +0100

    moved views creation into LogUser.sql

--- SQL/LogUser.sql
+++ SQL/LogUser.sql
@@ -209,3 +209,38 @@ create table XMLConfigs(
 	ParmVal varchar2(4000)
 );
 alter table XMLConfigs add constraint XMLConfigs_PK primary key(SimulationId, ProcessId, ThreadId, FileId, ParmId) using index tablespace LogIdx;
+
+
+-- Views
+create or replace  view vLastMSE as select v.processId, v.threadId, t.mse_t LastMSE
+from trainLog t, 
+(
+	select processid, threadid, max(epoch) LastEpoch from trainlog group by processid, threadid
+) v 
+where t.processid=v.processid and t.threadid=v.threadid and t.epoch=v.LastEpoch order by 1,2;
+--
+create or replace view vRunStats   as select processId, threadId, netProcessId, netThreadId, max(posLabel) LastDate, max(pos) LastPos , avg(BarWidth) avgBarWidth, avg(Error) avgError, avg(ErrorTRS*ErrorTRS) MSE from runlog group by processId, threadId, netProcessId, netThreadId;
+--
+create or replace view vXMLconfig as
+select processid, min(uHistoryLen) HistoryLen, min(uTopology) Topology, min(uMaxEpochs) MaxEpochs, avg(uMaxK) MaxK, min(uSampleLen) SampleLen, min(uDate0) Date0, min(uSymbol) Symbol, min(uTimeFrame) TimeFrame, min(uDataTransformation) DataTransformation from (
+select processid, to_number(replace(parmval,chr(0),''),'999999') uHistoryLen, NULL uTopology, NULL uMaxEpochs, NULL umaxK, NULL uSampleLen, NULL uDate0, NULL uSymbol, NULL uTimeFrame, NULL uDataTransformation from xmlconfigs where upper(parmdesc) like '%HISTORYLEN%'
+union
+select processid, NULL uHistoryLen, substr(parmval,1,256) uTopology, NULL uMaxEpochs, NULL umaxK, NULL uSampleLen, NULL uDate0, NULL uSymbol, NULL uTimeFrame, NULL uDataTransformation  from xmlconfigs where upper(parmdesc) like '%LEVELRATIO%'
+union
+select processid, NULL uHistoryLen, NULL uTopology, to_number(replace(parmval,chr(0),''),'99999') uMaxEpochs, NULL umaxK, NULL uSampleLen, NULL uDate0, NULL uSymbol, NULL uTimeFrame, NULL uDataTransformation  from xmlconfigs where upper(parmdesc) like '%MAXEPOCHS%'
+union
+select processid, NULL uHistoryLen, NULL uTopology, NULL uMaxEpochs, to_number(replace(parmval,chr(0),''),'99999') uMaxK, NULL uSampleLen, NULL uDate0, NULL uSymbol, NULL uTimeFrame, NULL uDataTransformation  from xmlconfigs where upper(parmdesc) like '%MAXK%'
+union
+select processid, NULL uHistoryLen, NULL uTopology, NULL uMaxEpochs, NULL umaxK, to_number(replace(parmval,chr(0),''),'99999') uSampleLen, NULL uDate0, NULL uSymbol, NULL uTimeFrame, NULL uDataTransformation  from xmlconfigs where upper(parmdesc) like '%SAMPLELEN%'
+union
+select processid, NULL uHistoryLen, NULL uTopology, NULL uMaxEpochs, NULL umaxK, NULL uSampleLen, substr(parmval,1,256) uDate0, NULL uSymbol, NULL uTimeFrame, NULL uDataTransformation  from xmlconfigs where upper(parmdesc) like '%DATE0%'
+union
+select processid, NULL uHistoryLen, NULL uTopology, NULL uMaxEpochs, NULL umaxK, NULL uSampleLen, NULL uDate0, substr(parmval,1,256) uSymbol, NULL uTimeFrame, NULL uDataTransformation  from xmlconfigs where upper(parmdesc) like '%SYMBOL%'
+union
+select processid, NULL uHistoryLen, NULL uTopology, NULL uMaxEpochs, NULL umaxK, NULL uSampleLen, NULL uDate0, NULL uSymbol, substr(parmval,1,256) uTimeFrame, NULL uDataTransformation from xmlconfigs where upper(parmdesc) like '%TIMEFRAME%'
+union
+select processid, NULL uHistoryLen, NULL uTopology, NULL uMaxEpochs, NULL umaxK, NULL uSampleLen, NULL uDate0, NULL uSymbol, NULL uTimeFrame, substr(parmval,1,256) uDataTransformation  from xmlconfigs where upper(parmdesc) like '%DATATRANSFORMATION%'
+) 
+group by processid
+;
+--
