b52930f07d01252c5019667447f6caad59a4a879
gcaglion <gcaglion@GCAGLION-ASUS>
2019-09-24 22:19:57 +0200

    fixed deletePid.sql

--- SQL/deletePid.sql
+++ SQL/deletePid.sql
@@ -1,11 +1,11 @@
 set verify on
 undefine pid
 delete from CLIENTINFO where processid in(&&pid);
-delete from COREIMAGE_NN where processid in(&&pid);
+delete from COREIMAGE_NN_N where processid in(&&pid);
 delete from COREIMAGE_NN_W where processid in(&&pid);
 delete from CORELAYOUTS where enginepid in(&&pid);
-delete from CORELOGGERPARMS where enginepid in(&&pid);
-delete from CORENNINTERNALSSCGD where enginepid in(&&pid);
+delete from CORELOGGERPARMS where processid in(&&pid);
+delete from CORENNINTERNALSSCGD where processid in(&&pid);
 delete from CORENNPARMS where processid in(&&pid);
 delete from DBCONNECTIONS where processid in(&&pid);
 delete from ENGINECORES where enginepid in(&&pid);
